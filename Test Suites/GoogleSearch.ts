c4f8a7bdd5fcc5b97c68c2f32adde17060b0d646
Vijin S <vijin.s@TA.COM>
2022-04-13 17:06:40 +0530

    Google search Project details added

---
+++ Test Suites/GoogleSearch.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>GoogleSearch</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>19c17d8c-6b55-4899-8f15-1ea37be92e4e</testSuiteGuid>
+   <testCaseLink>
+      <guid>e35638c3-766b-48c0-b201-612fe95f782e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TestCase1</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
